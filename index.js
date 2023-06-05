e070bfdf1a635db6d2a857a0fd5f3a7d072a90d0
vikas dakle <vikas@gmail.com>
2023-06-05 13:03:45 +0530

    dxsj dx

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin: "http://localhost:3000",
+    origin: "https://driving-booking-production.up.railway.app",
     credentials: true
 }))
 
